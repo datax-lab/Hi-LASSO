40757392093ca0a916b34ca56f8d035a684fed79
JongKown Jo <41224905+JJongKwon@users.noreply.github.com>
2021-08-16 11:41:52 +0900

    Update hi_lasso.py
    Upgrade version to 1.0.5

--- hi_lasso/hi_lasso.py
+++ hi_lasso/hi_lasso.py
@@ -1,6 +1,6 @@
 # Author: Jongkwon Jo <jongkwon.jo@gmail.com>
 # License: MIT
-# Date: 3, Jun 2020
+# Date: 10, Aug 2021
 
 import warnings
 
@@ -8,12 +8,10 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from . import util, glmnet_model
 from concurrent.futures import ProcessPoolExecutor
-from scipy.stats import norm, binom
+from scipy.stats import binom
 from tqdm import tqdm
 import numpy as np
-import binascii
 import math
-import os
 
 
 class HiLasso:
@@ -34,30 +32,27 @@ class HiLasso:
     
     Parameters
     ----------        
-    X: array-like of shape (n_samples, n_predictors)
-       predictor variables    
-    y: array-like of shape (n_samples,)
-       response variables    
     q1: 'auto' or int, optional [default='auto']
         The number of predictors to randomly selecting in Procedure 1.
         When to set 'auto', use q1 as number of samples.
     q2: 'auto' or int, optional [default='auto']
         The number of predictors to randomly selecting in Procedure 2.
         When to set 'auto', use q2 as number of samples.        
-    alpha: float [default=0.95]
-        confidence level for determination of bootstrap sample size.
-    d: float [default=0.05]
-        sampling error for determination of bootstrap sample size.
-    B: 'auto' or int, optional [default='auto']
-        The number of bootstrap samples.
-        When to set 'auto', B is determined by statistical strategy(using alpha and d).   
-    par_opt: Boolean [default=False]
+    L: int [default=30]
+       The expected value at least how many times a predictor is selected in a bootstrapping.            
+    alpha: float [default=0.05]
+       significance level used for significance test for feature selection
+    logistic: Boolean [default=False]
+        Whether to apply logistic regression model. 
+        For classification problem, Hi-LASSO can apply the logistic regression model.
+    random_state : int or None, optional [default=None]
+        If int, random_state is the seed used by the random number generator; 
+        If None, the random number generator is the RandomState instance used by np.random.default_rng
+    parallel: Boolean [default=False]
         When set to 'True', use parallel processing for bootstrapping.
-    max_workers: 'None' or int, optional [default='None']
-        The number of cores to use for parallel processing.
-        If max_workers is None or not given, it will default to the number of processors on the machine.
-    random_state: int, RandomState instance, [default='None']
-        The seed of the pseudo random number generator that selects a random feature to update.
+    n_jobs: 'None' or int, optional [default='None']
+        The number of CPU cores used when parallelizing.
+        If n_jobs is None or not given, it will default to the number of processors on the machine.
         
     
     Attributes
@@ -70,29 +65,27 @@ class HiLasso:
     Examples
     --------
     >>> from hi_lasso import HiLasso
-    >>> model = HiLasso(X, y)
-    >>> model.fit(significance_level=0.05)
+    >>> model = HiLasso(q1='auto', q2='auto', L=30, logistic=False, random_state=None, parallel=False, n_jobs=None)
+    >>> model.fit(X, y, sample_weight=None, significance_level=0.05)
     
     >>> model.coef_
+    >>> model.intercept_ 
     >>> model.p_values_
-    >>> model.selected_var_ 
     """
 
-    def __init__(self, X, y, q1='auto', q2='auto', B='auto', d=0.05, alpha=0.95, par_opt=False, max_workers=None, random_state=None):
-        self.n, self.p = X.shape
-        self.X = np.array(X)
-        self.y = np.array(y).ravel()
-        self.q1 = self.n if q1 == 'auto' else q1
-        self.q2 = self.n if q1 == 'auto' else q2
-        self.d = d
+    def __init__(self, q1='auto', q2='auto', L=30, alpha=0.05, logistic=False, random_state=None, parallel=False,
+                 n_jobs=None):
+        self.q1 = q1
+        self.q2 = q2
+        self.L = L
         self.alpha = alpha
-        self.B = math.ceil(norm.ppf((1-self.alpha)/2, loc=0, scale=1) ** 2 * self.q1 / self.p * (
-                1 - self.q1 / self.p) / self.d ** 2) if B == 'auto' else B
-        self.par_opt = par_opt
-        self.max_workers = max_workers
+        self.logistic = logistic
         self.random_state = random_state
+        self.parallel = parallel
+        self.n_jobs = n_jobs
 
-    def fit(self, significance_level=0.05, sample_weight=None):
+    def fit(self, X, y, sample_weight=None):
+
         """Fit the model with Procedure 1 and Procedure 2. 
         
         Procedure 1: Compute importance scores for predictors. 
@@ -100,49 +93,61 @@ class HiLasso:
         Procedure 2: Compute coefficients and Select variables.
         
         Parameters
-        ----------      
-        significance_level : float [default=0.05]
-            Criteria used for selecting variables.
+        ----------
+        X: array-like of shape (n_samples, n_predictors)
+           predictor variables    
+        y: array-like of shape (n_samples,)
+           response variables            
         sample_weight : array-like of shape (n_samples,), default=None
             Optional weight vector for observations. If None, then samples are equally weighted.
 
         Attributes
         ----------                
         coef_ : array
-            Estimated coefficients by Hi-LASSO.
+            Coefficients of Hi-LASSO.
         p_values_ : array
             P-values of each coefficients.
-        selected_var_: array
-            Selected variables by significance test.
-            
+        intercept_: float
+            Intercept of Hi-LASSO.
+           
         Returns
         -------
         self : object         
         """
-
+        self.X = np.array(X)
+        self.y = np.array(y).ravel()
+        self.n, self.p = X.shape
+        self.q1 = self.n if self.q1 == 'auto' else self.q1
+        self.q2 = self.n if self.q2 == 'auto' else self.q2
         self.sample_weight = np.ones(
             self.n) if sample_weight is None else np.asarray(sample_weight)
         self.select_prob = None
 
         print('Procedure 1')
-        beta1 = self._bootstrapping(mode='procedure1')
-        importance_score = np.nanmean(np.abs(beta1), axis=1)
-        importance_score = np.where(
-            importance_score == 0, 1e-10, importance_score)
+        b1 = self._bootstrapping(mode='procedure1')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            b1_mean = np.nanmean(np.abs(b1), axis=1)
+        importance_score = np.where(b1_mean == 0, 1e-10, b1_mean)
+
+        # rescaled to sum to number of features.
         self.select_prob = importance_score / importance_score.sum()
+        self.penalty_weights = 1 / (self.select_prob * 100)
 
         print('Procedure 2')
-        beta2 = self._bootstrapping(mode='procedure2')
-        self.coef_ = np.nanmean(beta2, axis=1)
-        self.p_values_ = self._compute_p_values(beta2)
-        self.selected_var_ = np.where(
-            self.p_values_ < significance_level / self.p, np.nanmean(beta2, axis=1), 0)
+        b2 = self._bootstrapping(mode='procedure2')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            b2_mean = np.nanmean(b2, axis=1)
+        self.p_values_ = self._compute_p_values(b2)
+        self.coef_ = np.where(self.p_values_ < self.alpha, b2_mean, 0)
+        self.intercept_ = np.average(self.y) - np.average(self.X, axis=0) @ self.coef_
         return self
 
     def _bootstrapping(self, mode):
         """
-        Apply different methods and q according to mode parameter.
-        Apply parallel processing according to par_opt parameter.
+        Apply different methods and q according to 'mode' parameter.
+        Apply parallel processing according to 'parallel' parameter.
         """
         if mode == 'procedure1':
             self.q = self.q1
@@ -150,9 +155,10 @@ class HiLasso:
         else:
             self.q = self.q2
             self.method = 'AdaptiveLASSO'
+        self.B = math.floor(self.L * self.p / self.q)
 
-        if self.par_opt:
-            with ProcessPoolExecutor(max_workers=self.max_workers) as executor:
+        if self.parallel:
+            with ProcessPoolExecutor(max_workers=self.n_jobs) as executor:
                 results = tqdm(executor.map(self._estimate_coef,
                                             np.arange(self.B)), total=self.B)
                 betas = np.array(list(results)).T
@@ -169,28 +175,26 @@ class HiLasso:
         """
         # Initialize beta : p by 1 matrix.
         beta = np.empty(self.p)
-        
         # Initialize beta into NANs.
         beta[:] = np.NaN
-        
-        # Set a random seed for each bootstrapping.
-        seed = (bootstrap_number + self.random_state if self.random_state else int(binascii.hexlify(os.urandom(4)), 16))        
-        rs = np.random.RandomState(seed)
-        
+        # Set random seed as each bootstrap_number.
+        rs = np.random.RandomState(
+            bootstrap_number + self.random_state) if self.random_state else np.random.default_rng()
         # Generate bootstrap index of sample and predictor.
-        bst_sample_idx = rs.choice(
-            np.arange(self.n), size=self.n, replace=True, p=None)
-        bst_predictor_idx = rs.choice(
-            np.arange(self.p), size=self.q, replace=False, p=self.select_prob)
-        
+        bst_sample_idx = rs.choice(np.arange(self.n), size=self.n, replace=True, p=None)
+        bst_predictor_idx = rs.choice(np.arange(self.p), size=self.q, replace=False, p=self.select_prob)
+
         # Standardization.
         X_sc, y_sc, x_std = util.standardization(self.X[bst_sample_idx, :][:, bst_predictor_idx],
                                                  self.y[bst_sample_idx])
-        
-        # Estimate coefficients.
-        coef = glmnet_model.ElasticNet(X_sc, y_sc, sample_weight=self.sample_weight) if self.method == 'ElasticNet' \
-            else glmnet_model.AdaptiveLasso(X_sc, y_sc, sample_weight=self.sample_weight,
-                                            weight_Adaptive=self.select_prob[bst_predictor_idx] * 100)
+        # Estimate coef.
+        if self.method == 'ElasticNet':
+            coef = glmnet_model.ElasticNet(X_sc, y_sc, logistic=self.logistic,
+                                           sample_weight=self.sample_weight[bst_sample_idx], random_state=rs)
+        else:
+            coef = glmnet_model.AdaptiveLasso(X_sc, y_sc, logistic=self.logistic,
+                                              sample_weight=self.sample_weight[bst_sample_idx], random_state=rs,
+                                              adaptive_weights=self.penalty_weights[bst_predictor_idx])
         beta[bst_predictor_idx] = coef / x_std
         return beta
 
